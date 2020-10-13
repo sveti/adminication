3be746254bf9ad22e665ffcdd632f61ca7595fcd
sveti <svetozara_stm@abv.bg>
2020-10-13 17:01:25 +0300

    Eureka server configured

---
+++ eurekaServer/src/main/java/f54148/adminication/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package f54148.adminication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
